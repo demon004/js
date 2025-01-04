97bca259a3ebb6853279e545f6de90ffb4fad61d
demon004 <63650069+demon004@users.noreply.github.com>
2025-01-04 06:58:27 +0000

    datatypes

---
+++ Code/datatypes.js
@@ -0,0 +1,11 @@
+console.log('datatypes');
+
+let name="Ritik";
+let age= 18
+let islogin = false
+
+console.log(typeof "name")
+console.log(typeof "age")
+console.log(typeof age)
+console.log(typeof null)    //object
+console.log(typeof undefined) //undefine
