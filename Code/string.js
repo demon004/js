6090f55e838d953d8edab9b0f427666c4f586814
demon004 <63650069+demon004@users.noreply.github.com>
2025-01-10 06:36:12 +0000

    string

---
+++ Code/string.js
@@ -0,0 +1,23 @@
+let name='ritik';
+let age= 24;
+
+// method 1
+console.log(name + " , age= " + age);
+
+// method2
+
+console.log(`${name} , age= ${age}`);
+
+const newstring = "This is my code for the practice";
+
+console.log(newstring.split(" "));
+console.log(newstring.concat("new word"));
+console.log(newstring);
+
+const objstr = new String("This is new string made using object");
+console.log(objstr.concat("newword"));
+console.log(objstr[2]);
+
+
+
+
